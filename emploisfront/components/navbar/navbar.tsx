6cb118976943481243cdc6f51f5818c656f8d5d4
Nirimo <faustin.legrand1@gmail.com>
2024-09-10 20:53:19 +0200

    Adding navbar components for the front

--- emploisfront/components/navbar/navbar.tsx
+++ emploisfront/components/navbar/navbar.tsx
@@ -5,7 +5,20 @@ import Image from 'next/image';
 
 const Navbar: React.FC = () => {
   return (
-    <p>SALUTTeeT</p>
+    <div className="w-[100%] flex justify-center items-center h-16">
+      <div className="flex flex-row justify-between items-center w-[80%]">
+        <div>
+          <p>AUE</p>
+        </div>
+        <div>
+          <a href="">Outils</a>
+        </div>
+        <div className="flex flex-row gap-4">
+          <a href="">Connexion</a>
+          <a href="">Inscription</a>
+        </div>
+      </div>
+    </div>
   )
 };
 
